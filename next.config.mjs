d8814c5b1bf9bd6299056f2a6166e6c246663677
sashauly <77851963+sashauly@users.noreply.github.com>
2025-06-10 17:39:42 +0300

    add pnpm to deploy confg, fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,22 @@
-/** @type {import('next').NextConfig} */
 import withPWAInit from '@ducanh2912/next-pwa';
 
 const withPWA = withPWAInit({
-  dest: 'public',
+  dest: 'public', // Where to output the service worker files
+  register: true, // Register the service worker
+  skipWaiting: true, // Skip waiting for service worker to activate
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in dev
 });
 
 const BASE_PATH = process.env.PAGES_BASE_PATH ?? '/resume-builder';
 
 const isProd = process.env.NODE_ENV === 'production';
 
-if (isProd) {
-  console.log('Running in production mode');
-  // Enable production-specific optimizations
-} else {
-  console.log('Running in development mode');
-  // Enable development-specific features
-}
-
-const nextConfig = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: false,
-  swcMinify: true,
   output: 'export',
   basePath: isProd ? BASE_PATH : '',
   assetPrefix: isProd ? BASE_PATH : '',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -35,6 +26,6 @@ const nextConfig = withPWA({
   images: {
     unoptimized: true,
   },
-});
+};
 
-export default nextConfig;
+export default withPWA(nextConfig);
